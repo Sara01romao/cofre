cdf7be93c6c41d33e348703b614c72601164e6f0
Sara Romão <sara-romao@live.com>
2022-06-01 23:37:37 -0300

    inicio pg login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,33 @@
+import { Link } from "react-router-dom";
+import Button from "../../components/Button";
+import { MainForm } from "./styles";
+import PigImg from "../../assets/images/pig.svg"
+
 
 
 
 
 export default function Login() {
+
+  function teste(){
+  console.log("Sara")
+  }
+
   return (
-    <div>Login
+    <MainForm>
+       <div>
+        <h1>Login</h1>
+        <input type="text" placeholder="Email..."/>
+        <input type="text" placeholder="Senha..."/>
+        <small>Esqueceu a senha?</small>
         
-    </div>
+        <Button txt="Entrar" ></Button>
+        <Link to="/createLogin">Criar conta</Link>
+
+      </div>
+      <img src={PigImg} alt="Ilustração  cofrinho " />
+
+    </MainForm>
+    
   )
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -6,3 +6,21 @@ import styled from  "styled-components";
 
 
 `; */
+
+export const MainForm =styled.main`
+ max-width: 1200px;
+ margin: 0 auto;
+ display: grid;
+ grid-template-columns: 1fr 1fr;
+ border: 1px solid red;
+ height: 100vh;
+ align-items: center;
+
+ div{
+     display: flex;
+     flex-direction: column;
+     gap: 20px;
+     padding: 30px;
+ }
+
+`

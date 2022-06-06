de087848a05fc25dda99ba5542931a58fe38d42b
Sara Romão <sara-romao@live.com>
2022-06-06 15:28:01 -0300

    ajuste padding

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const HeaderStyle=styled.header`
+    padding: 0px 15px;
     
     nav{
         max-width: 1200px;
         margin: 0 auto;
-        padding: 10px 0px;
+        padding: 10px 0px 5px;
+        
     }
 
 `

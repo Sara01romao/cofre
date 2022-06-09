7f8bddb8ba725b4f18eea661a41bd45251e3a040
Sara Romão <sara-romao@live.com>
2022-06-08 23:42:06 -0300

    inicio estrutura de criar conta

--- src/pages/CreateLogin/index.jsx
+++ src/pages/CreateLogin/index.jsx
@@ -1,8 +1,19 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import { Input } from '../../components/Input'
+import { MainCreate } from './styles'
 
 export  function CreateLogin() {
+  const [name, setName]= useState(null);
+  const [email, setEmail]=useState(null);
   return (
-    <div>Create Login</div>
+    <MainCreate>
+      <div>
+      <h1>Criar uma conta</h1>
+          <div>
+            <Input typeInput="text" setChange={setName} value={name} placeholder="Nome "/>
+          </div>
+      </div>
+    </MainCreate>
   )
 }

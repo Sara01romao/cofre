9f61a2bfd025995cb046aa12c68c18440f3e36ba
Sara Romão <sara-romao@live.com>
2022-06-12 23:03:19 -0300

    criação inputs

--- src/pages/CreateLogin/index.jsx
+++ src/pages/CreateLogin/index.jsx
@@ -6,12 +6,15 @@ import { MainCreate } from './styles'
 export  function CreateLogin() {
   const [name, setName]= useState(null);
   const [email, setEmail]=useState(null);
+  const [password, setPassword]= useState(null);
   return (
     <MainCreate>
       <div>
       <h1>Criar uma conta</h1>
           <div>
-            <Input typeInput="text" setChange={setName} value={name} placeholder="Nome "/>
+            <Input typeInput="text" id="Nome" setChange={setName} value={name} placeholder="Email"/>
+            <Input typeInput="text" id="Email" setChange={setEmail} value={email} placeholder="Email"/>
+            <Input typeInput="password" id="Senha" setChange={setPassword} value={password} placeholder="Senha"/>
           </div>
       </div>
     </MainCreate>

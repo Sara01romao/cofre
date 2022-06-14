cd95aa7e3c7f7683a4dfb35133c59a3a261fe08f
Sara Romão <sara-romao@live.com>
2022-06-13 22:45:57 -0300

    estrutura desktop

--- src/pages/CreateLogin/index.jsx
+++ src/pages/CreateLogin/index.jsx
@@ -1,22 +1,38 @@
 
-import React, { useState } from 'react'
-import { Input } from '../../components/Input'
-import { MainCreate } from './styles'
+import React, { useState } from 'react';
+import { Input } from '../../components/Input';
+import { MainCreate } from './styles';
+import ilustration from '../../assets/images/create.svg'
+import Button from '../../components/Button';
 
 export  function CreateLogin() {
   const [name, setName]= useState(null);
   const [email, setEmail]=useState(null);
   const [password, setPassword]= useState(null);
+  const [passwordConfirm, setPasswordConfirm]= useState(null)
   return (
     <MainCreate>
-      <div>
-      <h1>Criar uma conta</h1>
-          <div>
-            <Input typeInput="text" id="Nome" setChange={setName} value={name} placeholder="Email"/>
+     
+        <div className='infoCreate'>
+         
+          <img src={ilustration} alt="ilustração objetivos" />
+        </div>
+     
+         <div>
+          <h1>Criar uma conta</h1>
+          <form action="">
+            <Input typeInput="text" id="Nome" setChange={setName} value={name} placeholder="Nome"/>
             <Input typeInput="text" id="Email" setChange={setEmail} value={email} placeholder="Email"/>
             <Input typeInput="password" id="Senha" setChange={setPassword} value={password} placeholder="Senha"/>
-          </div>
-      </div>
+            <Input typeInput="password" id="Confimar Senha" setChange={setPasswordConfirm} value={passwordConfirm} placeholder="Confirma senha"/>
+            
+            <Button txt="Cadastrar"/>
+          </form>
+         </div>
+            
+            
+         
+      
     </MainCreate>
   )
 }
--- src/pages/CreateLogin/styles.js
+++ src/pages/CreateLogin/styles.js
@@ -3,7 +3,31 @@ import styled from "styled-components";
 export const MainCreate = styled.main`
 
  border: 1px solid red;
+ max-width: 1220px;
+ margin: 0 auto;
+ display: flex;
+ align-items: center;
+ height: calc(100vh - 79px);
 
+ div{
+     flex: 1;
+ }
+ 
+.infoCreate{
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+}
+
+ form{
+     flex: 1;
+     padding: 30px;
+     display: flex;
+     flex-direction: column;
+     gap: 10px;
+ }
 
 
 `;

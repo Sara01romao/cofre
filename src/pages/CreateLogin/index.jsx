634807c0cc47d9c3949bf6aad94b6b150cd29509
Sara Romão <sara-romao@live.com>
2022-06-16 00:00:30 -0300

    add cadastro localStorage

--- src/pages/CreateLogin/index.jsx
+++ src/pages/CreateLogin/index.jsx
@@ -6,10 +6,22 @@ import ilustration from '../../assets/images/create.svg'
 import Button from '../../components/Button';
 
 export  function CreateLogin() {
-  const [name, setName]= useState(null);
-  const [email, setEmail]=useState(null);
-  const [password, setPassword]= useState(null);
-  const [passwordConfirm, setPasswordConfirm]= useState(null)
+  const [name, setName]= useState('');
+  const [email, setEmail]=useState('');
+  const [password, setPassword]= useState('');
+  const [passwordConfirm, setPasswordConfirm]= useState('');
+
+  function handleCreate(event){
+    event.preventDefault();
+    const user =[{name:name, email:email, password:password}];
+    console.log(user)
+
+    localStorage.setItem('useCofre', JSON.stringify(user))
+   
+
+  }
+
+
   return (
     <MainCreate>
      
@@ -21,12 +33,12 @@ export  function CreateLogin() {
      
         <div className='form-content'>
         <h1>Criar uma conta</h1>
-        <form action="">
+        <form onSubmit={handleCreate} >
           <Input typeInput="text" id="Nome" setChange={setName} value={name} placeholder="Nome"/>
           <Input typeInput="text" id="Email" setChange={setEmail} value={email} placeholder="Email"/>
           <Input typeInput="password" id="Senha" setChange={setPassword} value={password} placeholder="Senha"/>
-          <Input typeInput="password" id="Confimar Senha" setChange={setPasswordConfirm} value={passwordConfirm} placeholder="Confirma senha"/>
-          
+{/*           <Input typeInput="password" id="Confimar Senha" setChange={setPasswordConfirm} value={passwordConfirm} placeholder="Confirma senha"/>
+ */}          
           <Button txt="Cadastrar"/>
         </form>
         </div>

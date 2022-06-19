0d0147f0be36fedbd7998618356878958c2d50a8
Sara Romão <sara-romao@live.com>
2022-06-18 23:35:09 -0300

    add form no login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -10,20 +10,19 @@ import PigSvg from "../../components/PigSvg";
 
 
 export default function Login() {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState('')
   const [email, setEmail] = useState('');
   const [passaword, setPassword] = useState('');
   let navigate = useNavigate();
 
 
  useEffect(()=>{
-   const userLocal= localStorage.getItem('useCofre');
-   setUser(JSON.parse(userLocal));
+   const storageUser= localStorage.getItem('useCofre');
    
-    
-   if(user){
+   if(storageUser){
      /* navigate('/home') */
-     console.log(user)
+     setUser(JSON.parse(storageUser))
+
    }
   
 
@@ -36,11 +35,14 @@ export default function Login() {
        <div className="inputContent">
          
           <h1>Login</h1>
+          <form>
           <Input typeInput="text" id="Email" setChange={setEmail} value={email} placeholder="Email"/>
           <Input typeInput="password" id="Senha" setChange={setPassword} value={passaword} placeholder="Senha"/>
           <small><Link to="/">Esqueceu a senha?</Link></small>
           
           <Button txt="Entrar"></Button>
+          </form>
+     
           <Link to="/createLogin">Criar conta</Link>
          
         

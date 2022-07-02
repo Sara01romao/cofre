bcdc33ae5e2d7fe98687794db9ecb677aed15e7c
Sara Romão <sara-romao@live.com>
2022-07-01 22:40:13 -0300

    verificação de senha e email

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -15,26 +15,42 @@ export default function Login() {
   const {userData, setUserData} = useContext(UserContext)
   const [user, setUser] = useState('')
   const [email, setEmail] = useState('');
-  const [passaword, setPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [ msgError, setMsgError] = useState(null);
   let navigate = useNavigate();
 
 
-/*  useEffect(()=>{
-   const storageUser= localStorage.getItem('useCofre');
+ useEffect(()=>{
+/*    const storageUser= localStorage.getItem('useCofre');
    
    if(storageUser){
      
      setUser(JSON.parse(storageUser))
 
-   }
+   } */
+  
   
 
- },[]) */
+ },[])
+
+ 
 
 
  function handleSubmit(e){
-   e.preventDefault
-    navigate('/home')
+   e.preventDefault()
+
+  if(userData !== null){
+    if(userData.email === email && userData.password === password){
+      setMsgError(null)
+     /*  navigate('/home')   */
+    }else if(userData.email != email || userData.password !== password){
+      setMsgError('Email ou senha incorretos')
+    }
+  }else{
+    setMsgError('usuario não existe')
+  }
+
+  
  }
 
  
@@ -46,8 +62,11 @@ export default function Login() {
           <h1>Login</h1>
           <form onSubmit={handleSubmit}>
             <Input typeInput="text" id="Email" setChange={setEmail} value={email} placeholder="Email"/>
-            <Input typeInput="password" id="Senha" setChange={setPassword} value={passaword} placeholder="Senha"/>
-            <small><Link to="/">Esqueceu a senha?</Link></small>
+            <Input typeInput="password" id="Senha" setChange={setPassword} value={password} placeholder="Senha"/>
+            <small>
+              {msgError != null ? msgError:''}
+              <Link to="/">Esqueceu a senha?</Link>
+              </small>
             
             <Button onClick={e => setName("sara")} txt="Entrar"></Button>
           </form>

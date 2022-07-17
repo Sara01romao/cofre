916476df7295cf1fcc4b8bf83be6c7e759ed07c4
Sara Romão <sara-romao@live.com>
2022-07-17 16:26:17 -0300

    spinner carregando

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,10 +2,11 @@ import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 import { MainForm } from "./styles";
 import { Input } from "../../components/Input";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import PigSvg from "../../components/PigSvg";
 import Logo from "../../assets/images/logo.svg"
 import { UserContext } from "../../context/user";
+import Loading from "../../components/Loading";
 
 
 
@@ -16,36 +17,23 @@ export default function Login() {
   const [password, setPassword] = useState('');
   const [ msgError, setMsgError] = useState(null);
   let navigate = useNavigate();
-
-
- useEffect(()=>{
-/*    const storageUser= localStorage.getItem('useCofre');
-   
-   if(storageUser){
-     
-     setUser(JSON.parse(storageUser))
-
-   } */
-  
-  
-
- },[])
-
- 
-
+  const [loding, setLoding] = useState(false);
 
  function handleSubmit(e){
    e.preventDefault()
-
+  setLoding(true);
   if(userData !== null){
     if(userData.email === email && userData.password === password){
       setMsgError(null)
+      setLoding(false);
      /*  navigate('/home')   */
     }else if(userData.email != email || userData.password !== password){
       setMsgError('Email ou senha incorretos')
+      setLoding(false);
     }
   }else{
     setMsgError('Usuário não existe')
+    setLoding(false);
   }
 
   
@@ -55,31 +43,67 @@ export default function Login() {
 
   return (
     <MainForm>
-      
-       <div className="inputContent">
-       <img className="logo" src={Logo} alt="logo"/>
-          <h1>Login</h1>
-          <form onSubmit={handleSubmit}>
-            <Input typeInput="text" id="Email" setChange={setEmail} value={email} placeholder="Email"/>
-            <Input typeInput="password" id="Senha" setChange={setPassword} value={password} placeholder="Senha"/>
-            <small>
-              {msgError != null ? msgError:''}
-            </small>
-            <small>
-              <Link to="/">Esqueceu a senha?</Link>
-            </small>
+      {loding ==! false ?
+      <>
+          <Loading/> 
+          <div className="inputContent">
+          <img className="logo" src={Logo} alt="logo"/>
+              <h1>Login</h1>
+              <form onSubmit={handleSubmit}>
+                <Input typeInput="text" id="Email" setChange={setEmail} value={email} placeholder="Email"/>
+                <Input typeInput="password" id="Senha" setChange={setPassword} value={password} placeholder="Senha"/>
+                <small>
+                  {msgError != null ? msgError:''}
+                </small>
+                <small>
+                  <Link to="/">Esqueceu a senha?</Link>
+                </small>
+                
+                <Button onClick={e => setName("sara")} txt="Entrar"></Button>
+              </form>
             
-            <Button onClick={e => setName("sara")} txt="Entrar"></Button>
-          </form>
-         
-          <Link to="/createLogin" className="link">Criar conta</Link>
-         
-      </div>
+              <Link to="/createLogin" className="link">Criar conta</Link>
+            
+          </div>
 
-      <div clasName="msgsContent">
-       
-        <PigSvg/>
-      </div>
+          <div clasName="msgsContent">
+          
+            <PigSvg/>
+          </div>
+      
+      </>
+      :
+
+      <>
+          <div className="inputContent">
+          <img className="logo" src={Logo} alt="logo"/>
+              <h1>Login</h1>
+              <form onSubmit={handleSubmit}>
+                <Input typeInput="text" id="Email" setChange={setEmail} value={email} placeholder="Email"/>
+                <Input typeInput="password" id="Senha" setChange={setPassword} value={password} placeholder="Senha"/>
+                <small>
+                  {msgError != null ? msgError:''}
+                </small>
+                <small>
+                  <Link to="/">Esqueceu a senha?</Link>
+                </small>
+                
+                <Button onClick={e => setName("sara")} txt="Entrar"></Button>
+              </form>
+            
+              <Link to="/createLogin" className="link">Criar conta</Link>
+            
+          </div>
+
+          <div clasName="msgsContent">
+          
+            <PigSvg/>
+          </div>
+      </>
+      
+      
+      }
+      
       
      
     </MainForm>

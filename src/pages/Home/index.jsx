67d588dc63ab68850a1e3a8723db8eced68484f0
Sara Romão <sara-romao@live.com>
2022-09-22 00:38:11 -0300

    incio menu páginas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,20 @@
 
-
+import {GrDocumentText, GrHome} from 'react-icons/gr'
+import { Link} from 'react-router-dom'
 import './styles.css'
-
-
 export function Home() {
 
   return (
    <div>
-     home
+     <header>
+       <nav>
+         <ul>
+           <li><Link to="/"><GrHome />Home</Link></li>
+           <li><Link to="/"></Link><GrDocumentText/> Histórico</li>
+         </ul>
+       </nav>
+       
+     </header>
    </div>
   )
 }
